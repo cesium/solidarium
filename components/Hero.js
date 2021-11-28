bfba0de557651432e14f39905ff96a41d5286b14
Luis <44007026+LAraujo7@users.noreply.github.com>
2021-11-28 18:43:58 +0000

    snowflakes

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Hero() {
   return(
     <div className="w-full h-full bg-primary">
-      <div className="flex flex-col items-center w-full pt-24 pb-64">
+      <div className="flex flex-col items-center w-full pt-32 pb-64">
         <Image src="/images/arvore.svg" alt="Logo" width={400} height={400}/>
         <p className="py-4 text-5xl text-white sm:text-6xl lg:text-7xl font-ontel">Dezembro Solidário</p>
         <p className="text-xs tracking-wider text-white font-poppins_light sm:text-sm sm:tracking-widest">Dezembro Solidário 2021 @ Universidade do Minho</p>
--- components/Snowfall.js
+++ components/Snowfall.js
@@ -1,7 +1,12 @@
 export default function Snowfall() {
   return(
     <>
-      {[...Array(10).keys()].map(key => <div key={key} className="snowflake">❄</div>)}
+      {[...Array(10).keys()].map(key => <div key={key} className="text-white snowflake">{
+        key % 2 == 0 ?  
+      '❅'
+      :
+      '❆'
+      }</div>)}
     </>
   )
 }
