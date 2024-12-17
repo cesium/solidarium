43177c03ae137fbcd6d351c3334777f135ec1ba3
Gustavo <carlosgsp39@gmail.com>
2024-12-17 21:25:27 +0000

    Information updated

--- components/Hero.js
+++ components/Hero.js
@@ -6,7 +6,8 @@ export default function Hero() {
       <div className="flex flex-col items-center w-full pt-32 pb-64">
         <Image src="/images/Assets/Solidarium-branco.svg" alt="Logo" width={400} height={400}/>
         <p className="py-4 text-5xl text-white sm:text-6xl lg:text-7xl font-ontel">SolidariUM</p>
-        <p className="text-xs tracking-wider text-white font-poppins_light sm:text-sm sm:tracking-widest">SolidariUM 2022 @ Universidade do Minho</p>
+        <p className="text-xs tracking-wider text-white font-poppins_light sm:text-sm sm:tracking-widest">Recolha de bens de 10 de dezembro a 10 de janeiro</p>
+        <p className="text-xs tracking-wider text-white font-poppins_light sm:text-sm sm:tracking-widest">Universidade do Minho</p>
       </div>
     </div>
   )
